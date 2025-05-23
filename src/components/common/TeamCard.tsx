1f0ff18496eb006bb78f5cb33b3dfb618249ec8f
jihansoft <jihanuddinsoft@gmail.com>
2025-05-23 19:06:56 +0600

    update card content

--- src/components/common/TeamCard.tsx
+++ src/components/common/TeamCard.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 interface TeamCardProps {
   name: string;
-  title: string;
+  // title: string;
   description?: string;
   emailLink?: string;
   bgColor?: string;
@@ -10,28 +10,28 @@ interface TeamCardProps {
 
 const TeamCard: FC<TeamCardProps> = ({
   name,
-  title,
+
   description,
   emailLink,
   bgColor = "bg-blue-500",
 }) => {
   return (
     <div
-      className={`lg:w-[200px] lg:h-[240px] md:w-[220px] md:h-[250px] sm:w-full sm:min-h-[250px] w-full min-h-[250px] ${bgColor} text-white rounded-[30px] flex flex-col justify-between  p-5`}>
+      className={`lg:w-[220px] lg:h-auto md:w-[220px] md:h-[250px] sm:w-full sm:min-h-[250px] w-full min-h-[250px] ${bgColor} text-white rounded-[30px] flex flex-col justify-between  p-5`}>
       <div>
         <h3 className="text-xl font-bold leading-tight">{name}</h3>
-        <p className="text-md mt-1">({title})</p>
+        {/* <p className="text-md mt-1">({title})</p> */}
       </div>
 
       {description && (
-        <div className="text-sm mt-5">
+        <div className="text-sm mt-3">
           {description.split("\n").map((line, i) => (
             <p key={i}>{line}</p>
           ))}
         </div>
       )}
 
-      <div className="mt-auto text-base font-semibold">
+      <div className="mt-3 text-base font-semibold">
         <a
           href={emailLink ? `mailto:${emailLink}` : "#"}
           className="inline-flex items-center gap-1 hover:underline">
--- src/components/pages-layout/Behindthebrand/BehindOurTeam.tsx
+++ src/components/pages-layout/Behindthebrand/BehindOurTeam.tsx
@@ -13,62 +13,62 @@ export default function BehindOurTeam() {
         <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
           <TeamCard
             name={t("teamsction.team1.name")}
-            title={t("teamsction.team1.position")}
-            description={t(`teamsction.team1.description`)}
+            // title={t("teamsction.team1.position")}
+            description={t("teamsction.team1.description")}
             emailLink="sales@company.com"
           />
           <TeamCard
             name={t("teamsction.team2.name")}
-            title={t("teamsction.team2.position")}
+            // title={t("teamsction.team2.position")}
             description={t("teamsction.team2.description")}
             emailLink="sales@company.com"
           />
           <TeamCard
             name={t("teamsction.team3.name")}
-            title={t("teamsction.team3.position")}
+            // title={t("teamsction.team3.position")}
             description={t("teamsction.team3.description")}
             emailLink="sales@company.com"
           />
           <TeamCard
             name={t("teamsction.team4.name")}
-            title={t("teamsction.team4.position")}
+            // title={t("teamsction.team4.position")}
             description={t("teamsction.team4.description")}
             emailLink="sales@company.com"
           />
           <TeamCard
             name={t("teamsction.team5.name")}
-            title={t("teamsction.team5.position")}
+            // title={t("teamsction.team5.position")}
             description={t("teamsction.team5.description")}
             emailLink="sales@company.com"
           />
           <TeamCard
             name={t("teamsction.team6.name")}
-            title={t("teamsction.team6.position")}
+            // title={t("teamsction.team6.position")}
             description={t("teamsction.team6.description")}
             emailLink="sales@company.com"
           />
           <TeamCard
             name={t("teamsction.team7.name")}
-            title={t("teamsction.team7.position")}
+            // title={t("teamsction.team7.position")}
             description={t("teamsction.team7.description")}
             emailLink="sales@company.com"
           />
           <TeamCard
             name={t("teamsction.team8.name")}
-            title={t("teamsction.team8.position")}
+            // title={t("teamsction.team8.position")}
             description={t("teamsction.team8.description")}
             emailLink="sales@company.com"
           />
 
           <TeamCard
             name={t("teamsction.team9.name")}
-            title={t("teamsction.team9.position")}
+            // title={t("teamsction.team9.position")}
             description={t("teamsction.team9.description")}
             emailLink="sales@company.com"
           />
           <TeamCard
             name={t("teamsction.team10.name")}
-            title={t("teamsction.team10.position")}
+            // title={t("teamsction.team10.position")}
             description={t("teamsction.team10.description")}
             emailLink="sales@company.com"
           />
