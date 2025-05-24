7b9de5fe2af50256673554f7d48b0e0e7822b1a7
jihansoft <jihanuddinsoft@gmail.com>
2025-05-24 17:38:16 +0600

    update hover color

--- src/components/common/TeamCard.tsx
+++ src/components/common/TeamCard.tsx
@@ -19,7 +19,7 @@ const TeamCard: FC<TeamCardProps> = ({
   return (
     <div
       onClick={onClick}
-      className={`lg:w-[220px] cursor-pointer lg:h-auto md:w-[220px] md:h-[250px] sm:w-full sm:min-h-[250px] w-full min-h-[250px] ${bgColor} text-white rounded-[30px] flex flex-col justify-between  p-5`}>
+      className={`lg:w-[220px] group duration-300 cursor-pointer lg:h-auto md:w-[220px] md:h-[250px] sm:w-full sm:min-h-[250px] w-full min-h-[250px] ${bgColor} text-white rounded-[30px] flex flex-col justify-between  p-5`}>
       <div>
         <h3 className="text-xl font-bold leading-tight">{name}</h3>
         {/* <p className="text-md mt-1">({title})</p> */}
@@ -33,10 +33,10 @@ const TeamCard: FC<TeamCardProps> = ({
         </div>
       )}
 
-      <div className="mt-3 bg-primary-secondary rounded-lg py-2 px-4 text-base font-semibold">
+      <div className="mt-3 bg-primary-secondary duration-300 group-hover:bg-primary rounded-lg py-2 px-4 text-base font-semibold">
         <a
           onClick={onClick}
-          className="inline-flex text-primary cursor-pointer items-center gap-1 hover:underline">
+          className="inline-flex text-primary duration-300 group-hover:text-white cursor-pointer items-center gap-1 hover:underline">
           {btnText}
         </a>
       </div>
