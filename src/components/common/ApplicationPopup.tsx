9728568acde61e15202fc7955269bc34b3b931e1
jihansoft <jihanuddinsoft@gmail.com>
2025-05-29 11:55:23 +0600

    update card pop update email or multi lang

--- src/components/common/ApplicationPopup.tsx
+++ src/components/common/ApplicationPopup.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { useTranslations } from "next-intl";
 import React from "react";
 
 interface Props {
   onClose: () => void;
 }
 export default function ApplicationPopup({ onClose }: Props) {
+  const t = useTranslations("behindthebrand.teamsction.applicationpopup");
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
       <div className="bg-white rounded-lg max-w-md w-full p-6 relative">
@@ -27,33 +29,25 @@ export default function ApplicationPopup({ onClose }: Props) {
           </svg>
         </button>
 
-        <h2 className="text-2xl font-bold text-gray-800 mb-4">
-          Ready to impress?
-        </h2>
+        <h2 className="text-2xl font-bold text-gray-800 mb-4">{t("title")}</h2>
 
         <div className="space-y-4 text-gray-600">
           <p>
-            Email your CV to{" "}
-            <a
-              className="font-semibold"
-              href="mailto:career@Recruitment4u.co"
-            >
-              career@Recruitment4u.co
+            {t("subtitle1")}{" "}
+            <a className="font-semibold" href="mailto:career@Recruitment4u.co">
+              career@Recruitment4u.online
             </a>{" "}
-            with your desired position in the subject line.
+            <span>{t("subtitle2")}.</span>
           </p>
-          <p>A cover letter helps us see what makes you shine!</p>
-          <p className="text-sm text-gray-500">
-            By applying, you consent to us storing your details for up to 24
-            months, in line with EU data protection laws.
-          </p>
+          <p>{t("desc1")}</p>
+          <p className="text-sm text-gray-500">{t("desc2")}</p>
         </div>
 
         <div className="mt-6 flex justify-end">
           <button
             onClick={onClose}
             className="px-4 cursor-pointer py-2 bg-primary-secondary text-primary  rounded-md hover:bg-primary hover:text-white duration-300">
-            Got it!
+            {t("btn")}
           </button>
         </div>
       </div>
