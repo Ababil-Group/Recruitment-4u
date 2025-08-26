6da9a3790cc78253a7c5e8a9038d3bc22491ea94
skrifatuiux <skrifatuiux@gmail.com>
2025-08-26 12:19:17 +0600

    update

--- src/components/common/ApplicationPopup.tsx
+++ src/components/common/ApplicationPopup.tsx
@@ -12,6 +12,7 @@ export default function ApplicationPopup({ onClose }: Props) {
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
       <div className="bg-white rounded-lg max-w-md w-full p-6 relative">
         <button
+          title="Close"
           onClick={onClose}
           className="absolute cursor-pointer top-4 right-4 text-gray-500 hover:text-gray-700">
           <svg
