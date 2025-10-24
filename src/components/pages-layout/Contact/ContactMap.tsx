1385358f8e87f41f1f7ffc0b69372f093d7b6300
jihansofts <jihanuddinsoft@gmail.com>
2025-10-24 18:19:13 +0600

    add adress contact pages

--- src/components/pages-layout/Contact/ContactMap.tsx
+++ src/components/pages-layout/Contact/ContactMap.tsx
@@ -31,6 +31,15 @@ export default function ContactMap() {
         mapEmbedUrl="https://maps.google.com/maps?width=600&height=400&hl=en&q=Global%20Business%20Center%2C%20Cornish%20Road%2C%20Doha%2C%20Qatar&t=&z=14&ie=UTF8&iwloc=B&output=embed" // Use the correct embed link
         mapLink="https://www.google.com/maps/place/Global+Business+Centre/@25.2792508,51.5492179,17z/data=!3m1!4b1!4m6!3m5!1s0x3e45c57a1e5989ad:0x735a51a8600ee447!8m2!3d25.279246!4d51.5517928!16s%2Fg%2F11c1qy6gws?entry=ttu&g_ep=EgoyMDI1MDQwMi4xIKXMDSoASAFQAw%3D%3D" // Optional
       />
+      <ContactMapCard
+        title={t("location4.title")}
+        address={t("location4.desc")}
+        // phone="+385 1 6064 420"
+        // email="info.hr@dekra.com"
+        hours="Workdays 08:30 – 16:30"
+        mapEmbedUrl="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2574.7642831663998!2d24.05906107688834!3d49.80929903332!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473ae95d17d9c567%3A0xde33b3705cfaafa1!2sViking%20Business%20Center!5e0!3m2!1sen!2sbd!4v1761308278802!5m2!1sen!2sbd" // Use the correct embed link
+        mapLink="https://www.google.com/maps/place/Viking+Business+Center/@49.809299,24.0590611,17z/data=!3m1!4b1!4m6!3m5!1s0x473ae95d17d9c567:0xde33b3705cfaafa1!8m2!3d49.8092956!4d24.061636!16s%2Fg%2F11pwptj52r?entry=ttu&g_ep=EgoyMDI1MTAyMC4wIKXMDSoASAFQAw%3D%3D" // Optional
+      />
     </div>
   );
 }
